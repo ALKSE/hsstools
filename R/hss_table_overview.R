b11264ad0157b82f5d20b4f762f561f155b0f670
RenRMT <github@renzeterpstra.com>
2022-04-14 15:22:03 +0200

    function overview tables

--- R/hss_table_overview.R
+++ R/hss_table_overview.R
@@ -1,19 +1,32 @@
 #' Create overview table
 #'
+#' @param df the data frame to use
 #' @param vars a character vector with relevant variable strings.
-#'
+#' @param type 'single' or 'multi'
 #' @return a dataframe
 #' @export
 #'
-hss_table_overview <- function(vars) {
+hss_table_overview <- function(df, vars, type) {
   table <- lapply(vars, function(x) {
-    df %>%
-      select(contains(x)) %>%
-      summarise(across(.fns = sum, na.rm = TRUE)) %>%
-      t()
+    if (type == "multi") {
+      df %>%
+        dplyr::select(contains(x)) %>%
+        dplyr::filter(if_all(everything(), ~ !is.na(.x))) %>%
+        dplyr::summarise(across(.fns = sum, na.rm = TRUE)) %>%
+        t()
+    } else if (type == "single") {
+      df %>%
+        dplyr::select(rlang::sym(x)) %>%
+        as_factor() %>%
+        dplyr::filter(if_all(everything(), ~ !is.na(.x))) %>%
+        group_by(!!rlang::sym(x), .drop = FALSE) %>%
+        summarise(n()) %>%
+        select(-rlang::sym(x))
+    } else {
+      warning(type, " is not a valid table type.")
+    }
   }) %>%
     bind_cols()
-
   names(table) <- vars
   return(table)
 }
