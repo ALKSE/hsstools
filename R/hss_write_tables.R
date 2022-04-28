acf0b545066d92b93316712079791e2504a2ddd2
RenRMT <github@renzeterpstra.com>
2022-04-28 14:53:00 +0200

    update to write Excel tables function

--- R/hss_write_tables.R
+++ R/hss_write_tables.R
@@ -7,14 +7,17 @@
 #' @return A named list of dataframes.
 #' @export
 #'
-hss_write_tables <- function(df, questions, group, p = TRUE) {
+hss_write_tables <- function(df, questions, group, percent = TRUE) {
   output_list <- lapply(questions, function(x) {
     tryCatch(
 
       if (names(questions[match(x, questions)]) == "select_one") {
-        x <- hss_table_single(df, x, group, percent = p)
+        x <- rbind(
+          hss_table_single(df, x, group, percent = percent),
+          c("Pval", hss_chisq(df, hss_lookup_list(x, TRUE), group, full = FALSE))
+        )
       } else if (names(questions[match(x, questions)]) == "select_multiple") {
-        x <- hss_table_multi(df, x, group, percent = p)
+        x <- hss_table_multi(df, x, group, percent = percent)
       },
       error = function(e) NULL
     )
