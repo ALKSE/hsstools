4da9ba2f5916cdeb344d6e4a42ae52caf9eabaf7
RenRMT <github@renzeterpstra.com>
2022-04-28 14:51:47 +0200

    updates to Excel table function

--- R/hss_export_tables.R
+++ R/hss_export_tables.R
@@ -8,21 +8,14 @@
 #'
 hss_export_tables <- function(df_list, path) {
   df_list <- df_list[-which(sapply(df_list, is.null))]
-
   sink(path)
 
   for (i in names(df_list)) {
-    if (names(df_list[[i]][ncol(df_list[[i]])]) == "p") {
-      padding_col <- 1
-    } else {
-      padding_col <- 0
-    }
+    cat(i, strrep(";", ncol(df_list[[i]])), "\n", sep = "")
 
-    cat(i, strrep(";", ncol(tables_list[[i]]) - padding_col), "\n", sep = "")
+    write.table(df_list[[i]], sep = ";", dec = ",", row.names = FALSE)
 
-    write.table(df_list[[i]], sep = ";", dec = ".", row.names = FALSE)
-
-    cat(strrep(";", ncol(tables_list[[i]]) - padding_col), "\n", sep = "")
+    cat(strrep(";", ncol(df_list[[i]])), "\n", sep = "")
   }
 
   sink()
