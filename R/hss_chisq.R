e6f740b617086fb506032f9fc02434c4f8dad510
RenRMT <github@renzeterpstra.com>
2022-05-02 16:50:25 +0200

    cleanup and documentation

--- R/hss_chisq.R
+++ R/hss_chisq.R
@@ -1,15 +1,32 @@
 #' Runs chi-squared test on selected variables
 #'
+#' This functions runs chi-squared significance tests for one or more variables
+#' in a dataframe with a single cross-variable. Variable names can be passed as a
+#' single object or as a character string.
+#'
 #' @param df The dataframe containing the variable(s) of interest
 #' @param vars The variable(s) of interest. Accepts a single value or character string.
 #' @param group The grouping (or disaggregation) variable.
 #' @param full should the full results be returned. If set to FALSE, only p.value is returned
 #'
-#' @return A vector containing the results of the chi-squared test for the selected variables.
+#' @return A vector containing the results of the chi-squared test for the selected
+#' variables. If full is set to TRUE this will be a list, otherwise an atomic numeric vector.
 #' @export
+#' @examples
+#' # Generate dummy data
+#' df <- hss_dummydata()
 #'
-hss_chisq <- function(df, vars, group, full = FALSE) {
+#' # Calculte p-value for chi-squared test on a 'select-one' question.
+#' pval_single <- hss_chisq(test, "singleresponse", "gender")
+#'
+#' # Calculate p-value for chi-squared test on a 'select-multiple' question.
+#' multi_options <- c("multi_option1", "multi_option2", "multi_option3")
+#' pval_multi <- hss_chisq(test, multi_options, "gender")
+#'
+#' # Chi-squared test with full output
+#' full <- hss_chisq(f, "singleresponse", full = TRUE)
 
+hss_chisq <- function(df, vars, group, full = FALSE) {
 
   if (full == FALSE) {
     x <- sapply(
@@ -22,21 +39,21 @@ hss_chisq <- function(df, vars, group, full = FALSE) {
         chisq.test(
           table(df[[x]], df[[group]])
         )[["p.value"]], digits = 3)
-        } else {0}, error = function(e) 0))
+        } else {NA}, error = function(e) NA))
       }
     )
-  } else {
-    x <- sapply(
+  } else if(full == TRUE){
+    x <- lapply(
       vars,
       function(x) {
         return(tryCatch(
           if(sum(!is.na(df[[x]])) > 0) {
-            chisq.test(
-              table(df[[x]], df[[group]])
-            )
-          } else {0}, error = function(e) 0))
+              chisq.test(
+                table(df[[x]], df[[group]])
+              )
+          } else {NA}, error = function(e) NA))
       }
     )
-  }
+  } else {stop("Invalid input for full: ", full)}
   return(x)
 }
