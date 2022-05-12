a50798c81e239b9f6b627e2af51a7fbcfdf1be7a
RenRMT <github@renzeterpstra.com>
2022-05-12 14:19:49 +0200

    documentation update

--- R/hss_write_tables.R
+++ R/hss_write_tables.R
@@ -3,6 +3,7 @@
 #' @param df The dataframe containing relevant variables
 #' @param questions A named character vector containing variable names and the table type required.
 #' @param group The desired grouping/disaggregation variable.
+#' @param percent Logical vector if the table should be created with percentage values. If set to FALSE, counts are shown.
 #'
 #' @return A named list of dataframes.
 #' @export
