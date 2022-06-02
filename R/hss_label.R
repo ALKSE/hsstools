ec1fb6db58b28c1aa5d482e31c7346a85dbc67c1
RenRMT <github@renzeterpstra.com>
2022-06-02 12:23:02 +0200

    label function updated to use new xlsform column names

--- R/hss_label.R
+++ R/hss_label.R
@@ -15,7 +15,7 @@ hss_label <- function(table, var) {
   }
   # apply labels
   table <- table %>%
-    flextable::add_header_lines(values = labels$question) %>%
+    # flextable::add_header_lines(values = labels$question) %>%
     flextable::mk_par(j = var, value = flextable::as_paragraph(
       flextable::as_chunk(labels$answers)
     ))
@@ -26,15 +26,14 @@ hss_label <- function(table, var) {
 .get_table_labels <- function(var, lang = "en") {
   # check for language input and define label column names
   if(!(lang == "en" | lang == "ar")) stop("invalid language input ", lang, ". Use \'en\' or \'ar\'.")
-  var_lang <- paste0("r_table_label_", lang)
-  val_lang <- paste0("r_table_label_", lang, "_column")
+  label_lang <- paste0("r_table_label_", lang)
 
-  # look up variable name in "r_name" column and return value from "r_table_label_en"
-  question <- var %>% .get_dict_varname("r_name", var_lang)
+  # look up variable name in "r_name" column and return value from "r_table_label_[language]"
+  question <- var %>% .get_dict_varname("r_name", label_lang)
   # look up "type" value for variable name. looks up "type" value in dict_val "list_name
   # and returns matching value labels from "r_table_label_en_columns
   answers <- var %>% .get_dict_varname("r_name", "type") %>%
-    .get_dict_valname("list_name", val_lang)
+    .get_dict_valname("list_name", label_lang)
   labels <- list(question = question, answers = answers)
   return(labels)
 }
