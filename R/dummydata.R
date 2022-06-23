263e2ebc71b29c5f4d334f4007afbb07cf26d6db
RenRMT <github@renzeterpstra.com>
2022-06-23 15:08:46 +0200

    dummy data description

---
+++ R/dummydata.R
@@ -0,0 +1,16 @@
+#' Dummy HSS data
+#'
+#' A dataset containing a number of common question types use by the HSS.
+#'
+#' @format A data frame with 437 rows and 21 variables:
+#' \describe{
+#'   \item{gender}{gender of the respondent}
+#'   \item{county}{county of the respondent}
+#'   \item{hhsize}{Number of peeple in respondent's household}
+#'   \item{occupation}{Respondent's occupation}
+#'   \item{occupation_wagetype}{Respondent's type of wage}
+#'   \item{residence}{Has respondent lived in this location for the past 5 years}
+#'   \item{migr_nr}{Number of times respondent has moved in the past 5 years}
+#'   \item{migr_why_...}{List of binary variables of reasons for migration}
+#' }
+"dummydata"
