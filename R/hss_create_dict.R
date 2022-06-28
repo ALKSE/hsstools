918e47c6636db1bbca6194bcfe01c6ca5397c09f
RenRMT <github@renzeterpstra.com>
2022-06-28 11:08:58 +0200

    Added location filtering parameter inside dictionary function

--- R/hss_create_dict.R
+++ R/hss_create_dict.R
@@ -8,11 +8,13 @@
 #' @param form Path to the XLS form.
 #' @param type Use "var" to create a dictionary of variable names and labels.
 #' Use "val" to create a dictionary of value names and labels.
+#' @param location For variable dictionaries, filters to the specified location to ensure that
+#' proper county/district labels are available.
 #' @return A dataframe containing variable or value names and their associated text labels.
 #' @export
 #'
 #' @rdname hss_create_dict
-hss_create_dict <- function(form, type) {
+hss_create_dict <- function(form, type, location = NULL) {
 
   if (type == "var") {
     dict <- readxl::read_xls(path = form, sheet = 1) %>%
@@ -25,6 +27,10 @@ hss_create_dict <- function(form, type) {
   } else if (type == "val") {
     dict <- readxl::read_xls(path = form, sheet = 2) %>%
       janitor::clean_names()
+    if(!is.null(location))
+      location <- tolower(location)
+    dict <- dplyr::filter(dict,
+                          .data$location == .env$location | is.na(.data$location))
   } else {
     stop(type, "is not a valid input type. Use \'var\' or \'val\'.")
   }
