a8c2228ed277e90cdceab9c2586059dfc1fb971c
RenRMT <github@renzeterpstra.com>
2022-09-26 10:15:28 +0200

    Updated hss_rename function

--- R/hss_rename.R
+++ R/hss_rename.R
@@ -1,169 +1,59 @@
-#' dplyr::rename HSS variables
+#' Rename HSS Survey variables
 #'
-#' This function is used to dplyr::rename the variables in the HSS dataset from the old
-#' Q... names to human-readable names. These new names are expected to be present
-#' in the XLS form for the dataset (both the variable names, and the suffixes for
-#' dplyr::select-multiple answer options.). The function also removes any non-question columns
-#' (notes etc.)
-#' @param df The dataframe containing the HSS survey questions
-#' @param dict_var Dictionary object of variable names created from the survey's XLS form
-#' @param dict_val Dictionary obect of value names created from the survey's XLS form.
+#' This function is used to rename the variables in an HSS dataset from the 'Q01'
+#' format to more human-readable variable names. This is done based on the XLS form
+#' for that particular survey round. The XLS form needs to have 'r_name' columns
+#' in both the question & response option sheets for this to work.
 #'
-#' @seealso `hss_create_dict()`
+#' @param dat the dataframe containing the HSS survey
+#' @param xlsform File path to the XLS form for this particular HSS survey.
 #'
-#' @return Returns a dataframe with names updated to new column names and non-question
-#' columns removed.
+#' @return Returns the dataframe with updated column names
 #' @export
-hss_rename <- function(df, dict_var, dict_val) {
-  # note: this function is based on a rather extensive R script that could probably
-  # refactored to be a lot more concise. Due to time constraints, for now the script
-  # has just been divided into several functions with this hss_rename function as a
-  # convenient wrapper.
-  out <- df %>%
-    dplyr::rename.select_one(dict_var) %>%
-    dplyr::rename.select_multiple(dict_val) %>%
-    dplyr::rename.select_multiple_repeat(dict_var, dict_val) %>%
-    drop.notes(dict_var)
+hss_rename = function(dat, xlsform) {
+  mapping = .load_xlsform(xlsform) %>%
+    .create_mapping()
+  dat = .apply_mapping(dat, mapping)
+  return(dat)
+}
+#' @keywords internal
+.load_xlsform <- function(file) {
+  form <- readxl::read_xls(file) %>%
+    janitor::clean_names()
+  form$type <- gsub("select_one |select_multiple ", "", form$type)
+
+  form_val <- readxl::read_xls(file, sheet = 2) %>%
+    janitor::clean_names()
+
+  out <- list(form = form, form_val = form_val)
   return(out)
 }
 #' @keywords internal
-rename.select_one <- function(data, dict_var) {
-  # dplyr::filter for all questions that have corresponding r_name
-  dvar_filtered <- dplyr::filter(dict_var, !is.na(r_name))
-  # match old & new name
-  name_match <- match(dvar_filtered$name, names(data))
-  names(data)[na.omit(name_match)] <- dvar_filtered$r_name[!is.na(name_match)]
+.create_mapping <- function(list) {
+  merged <- right_join(list$form %>% select(type, name, r_name),
+                       list$form_val %>% select(list_name, name, r_name),
+                       by = c("type" = "list_name")
+  ) %>% filter(!is.na(r_name.y))
 
-  return(data)
+  oldname2 <- paste(merged$name.x, merged$name.y, sep = "_") %>% unique()
 
+  newname2 <- paste(merged$r_name.x, merged$r_name.y, sep = "") %>% unique()
+
+  oldvar <- c(list$form$name, oldname2) %>% tolower()
+  newvar <- c(list$form$r_name, newname2) %>% tolower()
+
+  out <- data.frame(oldvar, newvar) %>%
+    mutate(newvar = if_else(is.na(newvar), oldvar, newvar)) %>%
+    filter(!is.na(oldvar))
+  return(out)
 }
 #' @keywords internal
-filter.dval <- function(dict_val) {
-  filtered <- dict_val %>%
-    dplyr::select(-label_english, -label_stata, -r_table_label_en, -location) %>%
-    dplyr::filter(
-      !is.na(r_name),
-      is.na(r_name_prefix)
-    ) %>%
-    dplyr::mutate(
-      varname = with(dict_var, name[match(list_name, type)])
-    ) %>%
-    dplyr::select(varname, everything(), -list_name) %>%
-    dplyr::mutate(
-      name_new = stringr::str_c(varname, "/", name)
-    ) %>%
-    dplyr::select(varname, name, name_new, everything())
-  return(filtered)
-}
-
-list.renamed <- function(dict_val) {
-  dval_filtered <- dplyr::filter.dval(dict_val)
-  list_done <- dval_filtered %>%
-    dplyr::select(varname) %>%
-    dplyr::rename(name = varname) %>%
-    dplyr::mutate(
-      status = "done"
-    )
-  return(list_done)
-}
-#' @keywords internal
-rename.select_multiple <- function(data, dict_val) {
-
-  filtered <- dict_val %>%
-    dplyr::select(-label_english, -label_stata, -r_table_label_en, -location) %>%
-    dplyr::filter(
-      !is.na(r_name),
-      is.na(r_name_prefix)
-    ) %>%
-    dplyr::mutate(
-      varname = with(dict_var, name[match(list_name, type)])
-    ) %>%
-    dplyr::select(varname, everything(), -list_name) %>%
-    dplyr::mutate(
-      name_new = stringr::str_c(varname, "/", name)
-    ) %>%
-    dplyr::select(varname, name, name_new, everything())
-
-  # dplyr::rename vars using sheet2
-  name_match <- match(filtered$name_new, names(data))
-  names(data)[na.omit(name_match)] <- filtered$r_name[!is.na(name_match)]
-
-  return(data)
-}
-#' @keywords internal
-rename.select_multiple_repeat <- function(data, dict_var, dict_val) {
-  list_done <- list.renamed(dict_val)
-
-  dval_filtered <- dict_val %>%
-    dplyr::select(-list_name, -label_english, -label_stata, -r_table_label_en, -location) %>%
-    dplyr::filter(!is.na(r_name_prefix)) %>%
-    tidyr::separate_rows(r_name_prefix, sep = " ") %>%
-    dplyr::mutate(
-      r_name2 = r_name,
-      r_name_middle2 = r_name_middle,
-      r_name_prefix2 = r_name_prefix
-    ) %>%
-    tidyr::unite(r_name, c(r_name_prefix, r_name_middle, r_name), sep = "", na.rm = TRUE) %>%
-    dplyr::rename(
-      r_name_suffix = r_name2,
-      r_name_middle = r_name_middle2,
-      r_name_prefix = r_name_prefix2
-    ) %>%
-    dplyr::relocate(r_name_suffix, .before = r_name_prefix) %>%
-    dplyr::relocate(r_name_prefix, .before = r_name_middle) %>%
-    dplyr::mutate(
-      r_name_base = r_name_prefix
-    ) %>%
-    tidyr::unite(r_name_base, c(r_name_prefix, r_name_middle), sep = "", na.rm = TRUE) %>%
-    dplyr::rename(
-      r_name_val = r_name
-    )
-
-
-  dvar_filtered <- dict_var %>%
-    dplyr::select(name, r_name) %>%
-    dplyr::filter(
-      !is.na(r_name)
-    ) %>%
-    dplyr::mutate(
-      r_name_all = r_name,
-      r_name2 = r_name
-    ) %>%
-    dplyr::left_join(list_done, by = "name") %>%
-    dplyr::filter(is.na(status)) %>%
-    dplyr::select(-status) %>%
-    dplyr::mutate(
-      r_name_all_temp = stringr::str_ends(r_name, "_all")
-    ) %>%
-    dplyr::filter(r_name_all_temp == TRUE) %>%
-    dplyr::select(-r_name_all_temp) %>%
-    dplyr::mutate(
-      r_name_all = str_remove(r_name_all, "_all")
-    ) %>%
-    dplyr::select(-r_name2) %>%
-    dplyr::rename(
-      name_var = name,
-      r_name_base = r_name_all
-    ) %>%
-    dplyr::ful_join(dval_filtered, by = "r_name_base") %>%
-    dplyr::mutate(
-      varname = paste(name_var, "/", name, sep = "")
-    ) %>%
-    dplyr::select(varname, r_name_val)
-
-
-  name_match <- match(dvar_filtered$varname, names(data))
-  names(data)[na.omit(name_match)] <- dvar_filtered$r_name_val[!is.na(name_match)]
-  return(data)
-}
-#' @keywords internal
-drop.notes <- function(data, dict_var) {
-  dvar_filtered <- dict_var %>%
-    dplyr::select(type, name) %>%
-    dplyr::filter(type=="note") %>%
-    dplyr::select(name)
-
-  data <- data[ , !(names(data) %in% dvar_filtered$name)]
-
-  return(data)
+.apply_mapping <- function(dat, mapping) {
+  names(dat) <- sapply(names(dat), function(x) {
+    if(length(mapping$newvar[which(mapping$oldvar == x)]) == 0) {x
+    } else {
+      mapping$newvar[which(mapping$oldvar == x)]
+    }
+  })
+  return(dat)
 }
