35c9a091af8dc1914a9bc11fb70b2df21ae90c8f
Latif <latif.sleibi@gmail.com>
2022-11-07 17:05:08 +0100

    Sanity check + comments

--- R/hss_rewrite.R
+++ R/hss_rewrite.R
@@ -9,9 +9,13 @@
 #' @export
 #'
 hss_rewrite <- function(dat){
-  prep <- readxl::read_xlsx("recode.xls")
+  #sanity check for recode
+  prep <<- readxl::read_xlsx("recode.xls")
+  #split dataset for looping
   y <- split(prep, f = row.names(prep))
+  #generate a stable way of indicating how the loop should operate (rows:1:n)
   z <- as.list(1:nrow(prep))
   for(i in z){
     dat[[as.character(y[[i]][[3]])]][dat[[as.character(y[[i]][[1]])]] == as.character(y[[i]][[2]])] <- as.numeric(y[[i]][[4]])}
+  #This is very ugly, but there is no immediate way of doing this better (maybe purr?)
   return(dat)}
