efdd50c0491e23ffd7fc4016314aea3b54af1b0c
RenRMT <github@renzeterpstra.com>
2022-11-08 13:52:44 +0100

    minor changes to hss_addlabels to work with updated hss_create_dict function

--- R/hss_addlabels.R
+++ R/hss_addlabels.R
@@ -5,30 +5,27 @@
 #' The second output is a list which will be used to match var_names in the data set to those in the DF
 #' The result of this function is a new *labelled data set
 #' @param data actual survey data set
-#' @param survey the master xls form
-#' @param location The location -directory- of the xls form
+#' @param dict A dictionary object created with hss_create_dict()
 #' @rdname hss_addlabels
 #' @export
 
-hss_addlabels <- function(survey, data, location){
-    dic_val_new <- hss_create_dict(survey, "val", location = location)
-    workfile <- subset(dic_val_new, select = c(list_name, r_name, label_english, name))
+hss_addlabels <- function(data, dict){
+
+    workfile <- subset(dict$val, select = c(list_name, r_name, label_english, name))
     workfile$final_name <- paste(workfile$list_name, workfile$r_name)
     workfile$final_name <- stringr::str_replace_all(workfile$final_name, "NA", "")
     workfile$final_name <- gsub(" ", "", workfile$final_name, fixed = TRUE)
-    form <- readxl::read_excel(survey)
-    new_labels <- subset(form, select = c(type, name, R_name))
+
+    new_labels <- subset(dict$var, select = c(type, name, r_name))
     new_labels <- na.omit(new_labels)
-    new_labels$type <- stringr::str_replace_all(new_labels$type, "select_one", "")
-    new_labels$type <- stringr::str_replace_all(new_labels$type, "select_multiple", "")
-    new_labels$type <- gsub(" ", "", new_labels$type, fixed = TRUE)
+
     names(new_labels)[1] <- "final_name"
     joined <- dplyr::inner_join(workfile, new_labels, by = "final_name")
-    labels_for <- unique(joined$R_name)
+    labels_for <- unique(joined$r_name.y)
     labels_for <- as.list(labels_for)
 
     for (i in labels_for) {
-      x <- dplyr::filter(joined, R_name == i)
+      x <- dplyr::filter(joined, r_name.y == i)
       data[[i]] <- factor(data[[i]],
                          levels= x$name.x,
                          labels= x$label_english)
