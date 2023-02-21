5f20c74686d71062da7ecf8be1e9764f712248f0
Abdullatif Sleibi <latif.sleibi@gmail.com>
2023-02-21 16:51:14 +0100

    fix for consistency

--- R/C_hss_surveyduration.R
+++ R/C_hss_surveyduration.R
@@ -5,16 +5,16 @@
 #'
 #' @param data The most recent iteration of the data set
 #'
-#' @return A dataframe with an added SurveyDuration column
+#' @return A dataframe with an added duration column
 #' @export
 #' @rdname C_hss_surveyduration
 C_hss_surveyduration <- function(data) {
   output <- data %>%
     dplyr::mutate(submission_date = lubridate::mdy_hms(submission_date)) %>%
     dplyr::mutate(start = lubridate::mdy_hms(start), end = lubridate::mdy_hms(end)) %>%
-    dplyr::mutate(SurveyDuration = end - start) %>%
-    dplyr::mutate(SurveyDuration = lubridate::as.duration(SurveyDuration)) %>%
-    dplyr::relocate(SurveyDuration, .after = end)
+    dplyr::mutate(duration = end - start) %>%
+    dplyr::mutate(duration = lubridate::as.duration(duration)) %>%
+    dplyr::relocate(duration, .after = end)
   return(output)
 }
 
