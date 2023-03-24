2cbf2f5ab736bac924b4a74d014a01263a8b6ba6
Abdullatif Sleibi <latif.sleibi@gmail.com>
2023-03-24 15:20:51 +0100

    Doing more splits

--- R/C_hss_NAgo.R
+++ R/C_hss_NAgo.R
@@ -3,9 +3,8 @@
 #' @param dat Data that needs to have blanks converted to NA
 #'
 #' @return Data that can be split
-#'
+#' @export
 #' @rdname C_hss_NAgo
-#'
 C_hss_NAgo <- function(dat){
   data_na <- dat %>% select_if(is.character)
   data_na_2 <- replace(data_na, data_na=='', NA)
