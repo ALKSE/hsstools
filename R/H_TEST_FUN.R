4f2cd6b65caab5062c7c1c0a993e8d904eea565f
Latif Sleibi <latif.sleibi@gmail.com>
2023-08-10 16:43:15 +0200

    multi_valname addition

--- R/H_TEST_FUN.R
+++ R/H_TEST_FUN.R
@@ -1,34 +1,20 @@
-#' TEST FUNCTION
+#' TEST FUNCTIONS
 #'
+#' This is a playground for functions under consideration.
 #'
 #' @export
 #'
-H_TEST_FUN <- function(df, questions, group, dict, percent = TRUE, digits = 1, lang = "en") {
-  out <- lapply(questions, function(questions_element) {
-    tryCatch(
-      if (names(questions[match(questions_element, questions)]) == "select_one") {
-        T_hss_table_single(df, questions_element, group, percent, digits) %>%
-          T_hss_label(questions_element, group, dict, lang) %>%
-          T_hss_format_single() %>%
-          flextable::add_footer_lines(
-            values = T_hss_chisq_formatted(df, questions_element, group)
-          )
-      },
-      error = function(e) NULL
-    )
-  })
-  # set names to appropriate question labels
-  names(out) <- sapply(
-    questions,
-    function(questions_element) {
-      .get_dict_varname(
-        questions_element,
-        "r_name",
-        paste0("r_table_label_", lang),
-        dict
-      )
-    }
-  )
 
-  return(out)
+#Generate the sub_names of a _all variable
+multi_valname <- function(var, df) {
+
+  answers <- names(
+    dplyr::select(
+      df,
+      dplyr::starts_with(
+        stringr::str_replace(var, "_all", "_")
+      ) & !dplyr::ends_with(c("_what", "_all"))
+    )
+  )
+  return(answers)
 }
