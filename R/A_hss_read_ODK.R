42d182dae0a9c515849650904cc23a3e6522e5aa
Latif Sleibi <latif.sleibi@gmail.com>
2023-09-06 20:20:29 +0200

    Function edit

--- R/A_hss_read_ODK.R
+++ R/A_hss_read_ODK.R
@@ -11,9 +11,8 @@
 #'
 #' @rdname A_hss_read_ODK
 #'
-A_hss_read_ODK <- function(dat){
-  audit <- read.csv("#1A_HSS_SS_2023_Jonglei - audit.csv")
-  audit_2 <- subset(audit, select = c(event, node, start, end, instance.ID))
+A_hss_read_ODK <- function(audit_dat){
+  audit_2 <- subset(audit_dat, select = c(event, node, start, end, instance.ID))
   names(audit_2)[5] <- "instance_id"
   return(audit_2)
 }
