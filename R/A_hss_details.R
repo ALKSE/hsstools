9e0f1dd8979557bbbc75c51b6a8ff2188fa8d65b
Latif Sleibi <latif.sleibi@gmail.com>
2025-09-02 12:09:44 +0200

    Edits (audit)

--- R/A_hss_details.R
+++ R/A_hss_details.R
@@ -59,8 +59,8 @@ A_hss_details <- function(dat, audit, name, max){
     filter(duration > 22)
 
   #This overview specifically presents the exclusions related to screen-time.
-  audit_proof_evidence <- subset(audit_proof_base, !audit_proof_base$instance_id %in%
-                                   audit_proof_advanced$instance_id)
+  audit_proof_evidence <- as.data.frame(subset(audit_proof_base, !audit_proof_base$instance_id %in%
+                                   audit_proof_advanced$instance_id))
 
 
   #Combine into a nice list
