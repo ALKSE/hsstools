83c693cf22e8781837d046206a6e66d9bf426003
Latif Sleibi <latif.sleibi@gmail.com>
2025-09-04 11:32:47 +0200

    rename update::TK::ALKS

--- R/C_hss_rename.R
+++ R/C_hss_rename.R
@@ -11,8 +11,17 @@
 #' @return Returns the dataframe with updated column names
 #' @export
 C_hss_rename <- function(dat, dict) {
+
   mapping <- .create_mapping(dict)
-  names(dat) <- .apply_mapping(names(dat), mapping)
+
+  #  Clean original column names
+  old_names <- janitor::make_clean_names(names(dat))
+
+  #  Apply mapping to get new variable names
+
+  names(dat) <- .apply_mapping(old_names, mapping)
+
+
   return(dat)
 }
 
@@ -35,24 +44,43 @@ C_hss_rename <- function(dat, dict) {
   # Create a mapping of old & new variable names based on provided XLS form. Output
   # is dataframe with columns 'oldvar' and 'newvar'
   merged <- dplyr::right_join(dict$var %>% dplyr::select(type, name, r_name),
-    dict$val %>% dplyr::select(list_name, name, r_name),
-    by = c("type" = "list_name")
-  ) %>% dplyr::filter(!is.na(r_name.y))
+                              dict$val %>% dplyr::select(list_name, name, r_name),
+                              by = c("type" = "list_name"), relationship = "many-to-many") %>%
+    dplyr::filter(!is.na(r_name.y))
 
+  #  Old name: e.g., q1_yes — for multiple choice questions
   oldname2 <- paste(merged$name.x, merged$name.y, sep = "_") %>% unique()
 
+  #  New readable name: e.g., wors_physical
   newname2 <- paste(merged$r_name.x, merged$r_name.y, sep = "") %>%
     gsub("_all_", "_", .) %>%
     unique()
 
+  #  Combine simple and compound names
   oldvar <- c(dict$var$name, oldname2) %>% tolower()
   newvar <- c(dict$var$r_name, newname2) %>% tolower()
 
-  out <- data.frame(oldvar, newvar) %>%
-    dplyr::mutate(newvar = dplyr::if_else(is.na(newvar), oldvar, newvar)) %>%
-    dplyr::filter(!is.na(oldvar))
+  #  Create final name mapping table
+  #Old code, consider reviewing one day
+  #out <- data.frame(oldvar, newvar) %>%
+    #dplyr::mutate(newvar = dplyr::if_else(is.na(newvar), oldvar, newvar)) %>%
+    #dplyr::filter(!is.na(oldvar))
+
+  out <- dict$var %>%
+    dplyr::filter(!is.na(r_name)) %>%
+    dplyr::select(name, r_name) %>%
+    dplyr::mutate(
+      oldvar = tolower(name),
+      newvar = tolower(r_name)
+    ) %>%
+    dplyr::select(oldvar, newvar) %>%
+    dplyr::mutate(
+      newvar = dplyr::if_else(is.na(newvar), oldvar, newvar)
+    )
+
   return(out)
 }
+
 #' @keywords internal
 .apply_mapping <- function(names, mapping) {
   # Replaces old names with new names. Accepts a character string as input. If an
